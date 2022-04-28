c3cd5613dbc3b23279d036dfaf5f30c2e7068a9a
unknown <mugishasamuel42@gmail.com>
2022-04-28 06:00:06 -0700

    Fix linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,38 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
- 
+RSpec.describe 'Users', type: :request do
   describe 'GET "/users" ' do
-    before (:example){ get '/users'}
+    before(:example) { get '/users' }
 
-    it "check if the response is successful" do
-         expect(response).to have_http_status(200)
+    it 'check if the response is successful' do
+      expect(response).to have_http_status(200)
     end
 
-    it "check if the correct template is rendered" do
-         expect(response).to render_template('index')
+    it 'check if the correct template is rendered' do
+      expect(response).to render_template('index')
     end
 
-    it "check If the response body includes correct placeholder text" do
+    it 'check If the response body includes correct placeholder text' do
       expect(response.body).to include('Here is a list of users')
     end
-
   end
 
   describe 'GET "/users/4" ' do
-    before (:example){ get '/users/4'}
+    before(:example) { get '/users/4' }
 
-    it "check if the response is successful" do
-         expect(response).to have_http_status(200)
+    it 'check if the response is successful' do
+      expect(response).to have_http_status(200)
     end
 
-    it "check if the correct template is rendered" do
-         expect(response).to render_template('show')
+    it 'check if the correct template is rendered' do
+      expect(response).to render_template('show')
     end
 
-    it "check If the response body includes correct placeholder text" do
+    it 'check If the response body includes correct placeholder text' do
       expect(response.body).to include('Here is a page for user 4')
     end
-
   end
 end
